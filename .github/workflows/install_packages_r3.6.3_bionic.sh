63cde5da8f94f62adfdbc6a9b6906df7d44c5ab8
Yuri05 <Yuri05@github.com>
2021-06-05 20:00:57 +0200

    ospsuite-10.0.25 , RE 1.2.40

--- .github/workflows/install_packages_r3.6.3_bionic.sh
+++ .github/workflows/install_packages_r3.6.3_bionic.sh
@@ -2,12 +2,12 @@
 
 # Get software
 wget -nv https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v0.8.3_Linux/rClr_0.8.3_Ubuntu18.tar.gz -P /tmp_setup/
-wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v9.1.4/ospsuite_9.1.4_ubuntu18.tar.gz -P /tmp_setup/
-wget -nv https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v1.1.1/tlf_1.1.1.tar.gz -P /tmp_setup/
-wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite.ReportingEngine/releases/download/v1.1.12/ospsuite.reportingengine_1.1.12.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v10.0.25/ospsuite_10.0.25_ubuntu18.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v1.2.4/tlf_1.2.4.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite.ReportingEngine/releases/download/v1.2.40/ospsuite.reportingengine_1.2.40.tar.gz -P /tmp_setup/
 
 # Install packages
 R CMD INSTALL /tmp_setup/rClr_0.8.3_Ubuntu18.tar.gz && \
-R CMD INSTALL /tmp_setup/tlf_1.1.1.tar.gz --install-tests && \
-R CMD INSTALL /tmp_setup/ospsuite_9.1.4_ubuntu18.tar.gz --install-tests && \
-R CMD INSTALL /tmp_setup/ospsuite.reportingengine_1.1.12.tar.gz --install-tests
+R CMD INSTALL /tmp_setup/tlf_1.2.4.tar.gz --install-tests && \
+R CMD INSTALL /tmp_setup/ospsuite_10.0.25_ubuntu18.tar.gz --install-tests && \
+R CMD INSTALL /tmp_setup/ospsuite.reportingengine_1.2.40.tar.gz --install-tests
--- .github/workflows/install_packages_r3.6.3_centos7.sh
+++ .github/workflows/install_packages_r3.6.3_centos7.sh
@@ -2,12 +2,13 @@
 
 # Get software
 wget -nv https://github.com/Open-Systems-Pharmacology/rClr/releases/download/v0.8.3_Linux/rClr_0.8.3_CentOS7.tar.gz -P /tmp_setup/
-wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v9.1.4/ospsuite_9.1.4_centOS7.tar.gz -P /tmp_setup/
-wget -nv https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v1.1.1/tlf_1.1.1.tar.gz -P /tmp_setup/
-wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite.ReportingEngine/releases/download/v1.1.12/ospsuite.reportingengine_1.1.12.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite-R/releases/download/v10.0.25/ospsuite_10.0.25_centOS7.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/TLF-Library/releases/download/v1.2.4/tlf_1.2.4.tar.gz -P /tmp_setup/
+wget -nv https://github.com/Open-Systems-Pharmacology/OSPSuite.ReportingEngine/releases/download/v1.2.40/ospsuite.reportingengine_1.2.40.tar.gz -P /tmp_setup/
+ls -la /tmp_setup/
 
 # Install packages
 R CMD INSTALL /tmp_setup/rClr_0.8.3_CentOS7.tar.gz && \
-R CMD INSTALL /tmp_setup/tlf_1.1.1.tar.gz --install-tests && \
-R CMD INSTALL /tmp_setup/ospsuite_9.1.4_centOS7.tar.gz --install-tests && \
-R CMD INSTALL /tmp_setup/ospsuite.reportingengine_1.1.12.tar.gz --install-tests
+R CMD INSTALL /tmp_setup/tlf_1.2.4.tar.gz --install-tests && \
+R CMD INSTALL /tmp_setup/ospsuite_10.0.25_centOS7.tar.gz --install-tests && \
+R CMD INSTALL /tmp_setup/ospsuite.reportingengine_1.2.40.tar.gz --install-tests
